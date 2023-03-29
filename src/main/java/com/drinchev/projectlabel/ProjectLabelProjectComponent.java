a6eb72158687862cf4de1622d1880d3745ad5469
Simon Gamma <github@survive.ch>
2023-03-29 16:14:58 +0200

    adapt when prefs change, modernize code

--- src/main/java/com/drinchev/projectlabel/ProjectLabelProjectComponent.java
+++ src/main/java/com/drinchev/projectlabel/ProjectLabelProjectComponent.java
@@ -1,8 +1,6 @@
 package com.drinchev.projectlabel;
 
-import com.drinchev.projectlabel.preferences.ApplicationPreferences;
-import com.drinchev.projectlabel.preferences.ProjectPreferences;
-import com.drinchev.projectlabel.resources.ui.StatusBarWidget;
+import com.drinchev.projectlabel.resources.ui.ProjectLabelStatusBarWidget;
 
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
@@ -12,39 +10,17 @@ import com.intellij.openapi.wm.WindowManager;
 public class ProjectLabelProjectComponent implements ProjectComponent {
 
     private Project project;
-    private ProjectPreferences projectPreferences;
-    private ApplicationPreferences applicationPreferences;
-    private StatusBar statusBar;
 
     public ProjectLabelProjectComponent(Project project) {
         this.project = project;
-        projectPreferences = ProjectPreferences.getInstance(project);
-        applicationPreferences = ApplicationPreferences.getInstance();
     }
 
     void onSettingsChanged() {
         StatusBar statusBar = WindowManager.getInstance().getStatusBar(project);
-        StatusBarWidget statusBarWidget = (StatusBarWidget) statusBar.getWidget(StatusBarWidget.WIDGET_ID);
+        ProjectLabelStatusBarWidget statusBarWidget = (ProjectLabelStatusBarWidget) statusBar.getWidget(ProjectLabelStatusBarWidget.WIDGET_ID);
         if (statusBarWidget != null) {
             statusBarWidget.rebuildWidget();
             statusBarWidget.updateUI();
         }
     }
-
-    @Override
-    public void projectOpened() {
-        statusBar = WindowManager.getInstance().getStatusBar(project);
-
-        if (statusBar != null) {
-            statusBar.addWidget(new StatusBarWidget(project, projectPreferences, applicationPreferences));
-        }
-    }
-
-    @Override
-    public void projectClosed() {
-        if (statusBar != null) {
-            statusBar.removeWidget(StatusBarWidget.WIDGET_ID);
-        }
-    }
-
 }
--- src/main/java/com/drinchev/projectlabel/ProjectLabelWidgetFactory.java
+++ src/main/java/com/drinchev/projectlabel/ProjectLabelWidgetFactory.java
@@ -2,7 +2,9 @@ package com.drinchev.projectlabel;
 
 import com.drinchev.projectlabel.preferences.ApplicationPreferences;
 import com.drinchev.projectlabel.preferences.ProjectPreferences;
+import com.drinchev.projectlabel.resources.ui.ProjectLabelStatusBarWidget;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.StatusBarWidget;
@@ -14,7 +16,7 @@ public class ProjectLabelWidgetFactory implements StatusBarWidgetFactory {
     @Override
     public @NotNull
     @NonNls String getId() {
-        return com.drinchev.projectlabel.resources.ui.StatusBarWidget.WIDGET_ID;
+        return ProjectLabelStatusBarWidget.WIDGET_ID;
     }
 
     @Override
@@ -30,15 +32,14 @@ public class ProjectLabelWidgetFactory implements StatusBarWidgetFactory {
 
     @Override
     public @NotNull StatusBarWidget createWidget(@NotNull Project project) {
-        System.out.println("createWidget");
         ProjectPreferences projectPreferences = ProjectPreferences.getInstance(project);
         ApplicationPreferences applicationPreferences = ApplicationPreferences.getInstance();
-        return new com.drinchev.projectlabel.resources.ui.StatusBarWidget(project, projectPreferences, applicationPreferences);
+        return new ProjectLabelStatusBarWidget(project, projectPreferences, applicationPreferences);
     }
 
     @Override
     public void disposeWidget(@NotNull StatusBarWidget statusBarWidget) {
-        System.out.println("disposeWidget");
+        Disposer.dispose(statusBarWidget);
     }
 
     @Override
